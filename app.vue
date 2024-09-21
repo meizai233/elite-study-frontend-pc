2b4accc3453fd001eb381467517e8b353a09915a
fengwanyan <fengwy1@chinatelecom.cn>
2024-09-21 13:04:20 +0800

    websocket测试

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <NuxtLayout>
+    <div @click="onClick" w-100px h100px bg-red>测试弹幕发送按钮</div>
     <NuxtPage :key="$route.fullPath" />
   </NuxtLayout>
 </template>
@@ -12,6 +13,19 @@ import "swiper/css/pagination";
 
 import "ant-design-vue/es/message/style/css";
 import "ant-design-vue/es/notification/style/css";
+import { io } from "socket.io-client";
+
+// 建立传输链接 http://127.0.0.1:8888
+const socket = io("ws://127.0.0.1:8888");
+// 发送弹幕事件
+const onClick = () => {
+  socket.emit("bulletChat", "测试弹幕");
+};
+
+// 监听message事件
+socket.on("message", (data) => {
+  console.log("服务器发送的消息", data);
+});
 
 const { asyncUserInfo } = $(useUser());
 // 同步用户数据
