5c47e11dc5effaed185db7c6dac5d1784057cef4
fengwanyan <fengwy1@chinatelecom.cn>
2025-02-17 17:25:11 +0800

    github login debug

--- api/github.ts
+++ api/github.ts
@@ -8,7 +8,7 @@ export async function getGitHubOauth() {
 
   // const redirect_uri = `http://localhost:8888/api/github_login/v1/redirect`;
   const authorize_uri = "https://github.com/login/oauth/authorize";
-  const auth_uri = `https://elitestudy.fun/api/api/github_login/v1/auth`;
+  const auth_uri = `https://elitestudy.fun/api/github_login/v1/auth`;
 
   // const link = `${authorize_uri}?client_id=${client_id}&redirect_uri=${redirect_uri}`;
 
